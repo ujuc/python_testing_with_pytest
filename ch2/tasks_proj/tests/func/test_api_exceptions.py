e1f9c8350bf7e09b4d5e4545bcb2758a7b6c65fe
Sungjin Kang <ujuc@ujuc.kr>
2018-08-17 20:00:48 +0900

    Add as single test class

--- ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -30,3 +30,18 @@ def test_get_raises():
     """get() should raise an exception with wrong type param."""
     with pytest.raises(TypeError):
         tasks.get(task_id='123')
+
+
+class TestUpdate():
+    """Test expected exceptions with tasks.update()."""
+
+    def test_bad_id(self):
+        """A non-int id should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id={'dict instead': 1},
+                         task=tasks.Task())
+
+    def test_bad_task(self):
+        """A non-Task task should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id=1, task='not a task')
