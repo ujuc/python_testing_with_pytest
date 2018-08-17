5dcb93fd5641b8a40b9575e1990f7c00296dc5da
Sungjin Kang <ujuc@ujuc.kr>
2018-08-17 19:06:51 +0900

    Add test_unique_id_2

---
+++ ch2/tasks_proj/tests/func/test_unique_id_2.py
@@ -0,0 +1,24 @@
+import pytest
+import tasks
+from tasks import Task
+
+
+@pytest.mark.skip(reason='misunderstood the API')
+def test_unique_id():
+    """Calling unique_id() twice should return different numbers."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
+
+
+def test_unique_id_2():
+    """unique_id() should return an unused id."""
+    ids = []
+    ids.append(tasks.add(Task('one')))
+    ids.append(tasks.add(Task('two')))
+    ids.append(tasks.add(Task('three')))
+    # grab a unique id
+    uid = tasks.unique_id()
+    # make sure it isn't in the list of existing ids
+    assert uid not in ids
+
