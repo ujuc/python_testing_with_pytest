2e6192eb77a9036596919f29252f417b2ef650bf
Sungjin Kang <ujuc@ujuc.kr>
2018-08-21 17:45:18 +0900

    Using Fixtures for test data

--- ch3/a/tasks_proj/tests/conftest.py
+++ ch3/a/tasks_proj/tests/conftest.py
@@ -3,6 +3,39 @@ import tasks
 from tasks import Task
 
 
+# Reminder of Task constructor interface
+# Task(summary=None, owner=None, done=False, id=None)
+# summary is required
+# owner and done are optional
+# id is set by database
+
+@pytest.fixture()
+def tasks_just_a_few():
+    """All summaries and owners are unique."""
+    return (
+        Task('Write some code.', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False)
+    )
+
+
+@pytest.fixture()
+def tasks_mult_per_owner():
+    """Several owners with several tasks each."""
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel')
+    )
+
 @pytest.fixture()
 def tasks_db(tmpdir):
     """Connect to db before tests, disconnect after."""
--- ch3/test_fixtures.py
+++ ch3/test_fixtures.py
@@ -10,3 +10,14 @@ def some_data():
 def test_some_data(some_data):
     """Use fixture return value in a test."""
     assert some_data == 42
+
+
+@pytest.fixture()
+def a_tuple():
+    """Return something more interesting."""
+    return (1, 'foo', None, {'bar': 23})
+
+
+def test_a_tuple(a_tuple):
+    """Demo the a_tuple fixture."""
+    assert a_tuple[3]['bar'] == 32
