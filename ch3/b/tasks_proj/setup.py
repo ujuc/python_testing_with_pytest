3e31504f2d5db236c81a2e87ecf97708e0ab416c
Sungjin Kang <ujuc@ujuc.kr>
2018-08-24 21:32:25 +0900

    Add install_requires in setup.py
    * 설치 패키지를 넣어주자.

--- ch3/b/tasks_proj/setup.py
+++ ch3/b/tasks_proj/setup.py
@@ -15,4 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    install_requires=[
+        'pytest', 'tinydb', 'pymongo',
+    ],
 )
