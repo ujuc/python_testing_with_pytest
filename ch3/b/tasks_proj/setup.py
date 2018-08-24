d747bd267df4f74f6be11fb9873fd6062b855f0f
Sungjin Kang <ujuc@ujuc.kr>
2018-08-24 21:25:32 +0900

    Edit setup.py
    * 잠시 딴대로 샘.
    * 패키징에 대한 부분은 따로 확인이 필요할듯.

--- ch3/b/tasks_proj/setup.py
+++ ch3/b/tasks_proj/setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/evn python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 import pkg_resources
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='tasks',
     version=pkg_resources.require('tasks')[0].version,
-    packages=['tasks'],
-    package_dir={'tasks': 'src/tasks'},
+    author='Thomas ujuc',
+    author_email='sungjin.kang@ujuc.me',
+    description='Pytest example Task App',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
 )
