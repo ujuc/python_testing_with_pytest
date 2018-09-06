e485f733bd7083b80b6fbaa25285884b328f6414
Sungjin Kang <ujuc@ujuc.kr>
2018-09-06 21:51:30 +0900

    Writing your own plugins - 3

--- ch5/b/tasks_proj/tests/conftest.py
+++ ch5/b/tasks_proj/tests/conftest.py
@@ -67,12 +67,21 @@ def tasks_db(tasks_db_session):
     tasks.delete_all()
 
 
+def pytest_addoption(parser):
+    """Turn nice features on with --nice option."""
+    group = parser.getgroup('nice')
+    group.addoption('--nice', action='store_true',
+                    help='nice: turn failures into')
+
+
 def pytest_report_header():
     """Thank tester for running tests."""
-    return "Thanks for running the tests."
+    if pytest.config.getoption('nice'):
+        return "Thanks for running the tests."
 
 
 def pytest_report_teststatus(report):
     """Turn failures into opportunities."""
-    if report.when == 'call' and report.failed:
-        return report.outcome, 'O', 'OPPORTUNITY for improvement'
+    if report.when == 'call':
+        if report.failed and pytest.config.getoption('nice'):
+            return report.outcome, 'O', 'OPPORTUNITY for improvement'
